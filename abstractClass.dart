bca9e2a8bc4592e97bc1664c023517d552d831b3
EdwardMelendezM <192666@unsaac.edu.pe>
2023-05-25 17:12:05 -0500

    adding implement and extends with the abstract class

--- abstractClass.dart
+++ abstractClass.dart
@@ -1,18 +1,54 @@
 void main(){
   // final windPlant = EnergyPlat();
+  final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
+
+  print("wind: ${ chargePhone(windPlant) }");
+  print("nuclear: ${chargePhone(nuclearPlant)}");
 }
+
+double chargePhone(EnergyPlat plant){
+  if(plant.energyLeft<10){
+    throw Exception('Not enought energy');
+  }
+  
+  return plant.energyLeft-10;
+}
+
 
 enum PlantType { nuclear,wind,water }
 
-  
+
 
 abstract class EnergyPlat{
   double energyLeft;
-  PlantType type;
+  final PlantType type;
 
   EnergyPlat({required this.energyLeft,required this.type});
 
-  void consumeEnery(double amout);
+  void consumeEnergy(double amout);
+}
 
+//Extends o implements
+class WindPlant extends EnergyPlat{
+  WindPlant({required double initialEnergy })
+    :super(energyLeft: initialEnergy, type: PlantType.wind);
 
+  @override
+  void consumeEnergy(double amout){
+    print("hELOO");
+  }
+}
+
+class NuclearPlant  implements EnergyPlat{
+  double energyLeft;
+  PlantType type=PlantType.nuclear;
+  NuclearPlant({
+    required this.energyLeft
+  });
+
+  @override
+  void consumeEnergy(double amout){
+    print("hELOO");
+  } 
 }
