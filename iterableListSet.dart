855befb3f1b9918837475bc16ca7146294ce969b
EdwardMelendezM <192666@unsaac.edu.pe>
2023-05-24 22:40:01 -0500

    adding iterable, list and set

---
+++ iterableListSet.dart
@@ -0,0 +1,13 @@
+void main(){
+  final numbers = [1,2,2,2,3,4,5,5,6,6,7,8,9,10];
+  print("This is the main");
+  print(numbers);
+  final reverseNumbers = numbers.reversed;
+  print("This is the reverse LIST ${reverseNumbers.toList()}");
+  print("This is the reverse SET  ${reverseNumbers.toSet()}");
+  
+  final numberGreaterThan5 = numbers.where((num){
+    return num>5 ;
+  });
+  print("new -> $numberGreaterThan5");
+}
